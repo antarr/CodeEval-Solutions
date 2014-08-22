6225851900760a3a4f39034f423e8cb97855dbf1
antarr byrd <antarrbyrd@computer.org>
2014-08-21 21:15:03 -0500

    update asciidecryption.rb

--- asciidecryption.rb
+++ asciidecryption.rb
@@ -1,8 +1,21 @@
+def find_key(len,ending, str)
+  str.split('40').each do |word|
+    if len.to_i && str.scan(word).count > 1
+      bytes = word.split(' ')
+      next unless bytes.count == len.to_i
+      return bytes.last.to_i - ending.strip.bytes.to_a.first.to_i
+    end
+  end
+end
+
 File.open(ARGV.first).each_line do |line|
-  size, last_char, *chars = line.chomp.gsub(/[\|]/,'').split(' ')
-  next unless size
-  chars.each do |c|
-    print "".concat(c.to_i) , ' '
-  end    
-  puts ''    
+  begin
+    len, terming_char, str = line.chomp.split('|')
+    next unless str
+    key = find_key(len, terming_char,str)
+    str.strip.split(/[ ]/).each{ |char| print "".concat(char.to_i-key.to_i) }
+    puts ''
+  rescue
+    puts ''
+  end
 end
