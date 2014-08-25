d1043ad8b8f6555fe391a6820f32d39b09301668
atbyrd <antarr.byrd@gmail.com>
2014-08-25 09:57:28 -0500

    Update repeatedsubstring.rb
    100% working!

--- repeatedsubstring.rb
+++ repeatedsubstring.rb
@@ -2,28 +2,25 @@ ARGV[0]= "repeated.txt"
 
 def substrings str
   pattern = '.'
-  subs = []
-  while pattern.length < str.length do
+  subs = []    
+  while pattern.length <= (str.length/2) do
     subs << str.scan(/(?=(#{pattern}))/)
-    pattern << '.'
-  end
-  subs.flatten.sort_by{ |s| "banana".index('#{s}')}
+    pattern << '.'  
+  end 
+  subs.flatten
 end
 
 File.open(ARGV[0]).readlines.each do |line|
+  next if line.strip!.empty?    
   longest = ""
-  current = line.strip
-  next unless current
-  if substrings(line) == substrings(line).uniq
+  subs = substrings(line).reject{ |r| r.strip.empty? }
+  if subs == subs.uniq
     puts "NONE"
     next
   end
-  line.split(/[ ]/).each do |word|
-    subs = substrings(word)
-    subs.each do |sub|
-      if line.scan(/#{sub}/).count > 1 && sub.length > longest.length
-        longest = sub
-      end
+  substrings(line).reject{ |r| r.strip.empty? }.each do |word|
+    if line.scan(/#{word}/).count > 1 && word.length > longest.length
+      longest = word
     end
   end
   puts longest == "" ? 'NONE' : longest
